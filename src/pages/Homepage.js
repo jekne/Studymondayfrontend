d3b376c275ecbf9455d3c3e2e4fc714dbc939bbc
Johann Eugen <jekneto@hotmail.com>
2021-11-01 23:55:47 +0100

    we have the home page showing the console the array

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,7 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
-
+// obj
+//fetch the players
+//
 const HomePage = () => {
+  useEffect(() => {
+    console.log("done rendering!");
+
+    const fetchPlayers = async () => {
+      try {
+        const response = await axios.get("http://localhost:4000/players");
+        console.log("this is my responseeeee", response.data);
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+    fetchPlayers();
+  });
   return (
     <div>
       <h1>This is my Home Page</h1>
