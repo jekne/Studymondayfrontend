424ae1e0c5f69114d75a60edf686487debb23a10
Johann Eugen <jekneto@hotmail.com>
2021-11-01 22:29:06 +0100

    we have the email and pword when type shows in the console

--- src/pages/Loginpage.js
+++ src/pages/Loginpage.js
@@ -6,17 +6,24 @@ import axios from "axios";
 //Button
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  console.log("form state", email, password);
+
   return (
     <div>
       <h1>This is my login page</h1>
       <form>
         <div>
           <label>EMAIL</label>
-          <input type="email" />
+          <input type="email" onChange={(e) => setEmail(e.target.value)} />
         </div>
         <div>
           <label>PASSWORD</label>
-          <input type="password"></input>
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
       </form>
     </div>
