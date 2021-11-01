449d5b7068be903c3c2ec69d294fff701a488877
Johann Eugen <jekneto@hotmail.com>
2021-11-01 22:17:04 +0100

    Created the passawor and the email login

--- src/pages/Loginpage.js
+++ src/pages/Loginpage.js
@@ -1,10 +1,24 @@
 import { useState } from "react";
 import axios from "axios";
+//HTML
+//<form>
+//<label>+<input onChange>
+//Button
 
 const LoginPage = () => {
   return (
     <div>
       <h1>This is my login page</h1>
+      <form>
+        <div>
+          <label>EMAIL</label>
+          <input type="email" />
+        </div>
+        <div>
+          <label>PASSWORD</label>
+          <input type="password"></input>
+        </div>
+      </form>
     </div>
   );
 };
